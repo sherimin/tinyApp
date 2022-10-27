36140896e37506e9d83266fb61863ea79d418f9b
Sheri Lu <sherisherilu@gmail.com>
2022-10-27 05:09:50 +0000

    URL Shortening

--- express_server.js
+++ express_server.js
@@ -2,20 +2,34 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+//POST request
+app.use(express.urlencoded({ extended: true }));
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 
 //pass the URL data to template
 app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
-    res.render("urls_index", templateVars);
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
-    const templateVars = { id: req.params.id, longURL: `urlDatabase`/* What goes here? */ };
-    res.render("urls_show", templateVars);
+  const templateVars = { id: req.params.id, longURL: `urlDatabase`/* What goes here? */ };
+  res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+
 
 
 const urlDatabase = {
@@ -35,3 +49,6 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+function generateRandomString() {
+    return Math.random().toString(36).slice(2,8);
+}
